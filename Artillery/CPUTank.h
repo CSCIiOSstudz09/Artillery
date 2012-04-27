9b00dac9f792c2caf2e1544ab30893274fe7299c
studz09 <joseph.studniarz@msu.montana.edu>
2012-04-27 15:46:05 -0600

    Commit 2

---
+++ Artillery/CPUTank.h
@@ -0,0 +1,13 @@
+//
+//  CPUTank.h
+//  FinalProject
+//
+//  Created by default on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Tank.h"
+
+@interface CPUTank : Tank
+
+@end
